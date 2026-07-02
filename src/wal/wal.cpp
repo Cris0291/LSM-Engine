dc03c8f8bcb00a7de01c5577fac7bb6fba77dbca
Cristian Blanco <cris0291@gmail.com>
2026-07-02 14:39:02 -0600

    improve encode and start implementing replay

--- src/wal/wal.cpp
+++ src/wal/wal.cpp
@@ -1,10 +1,20 @@
 #include "wal.h"
+#include <sys/types.h>
 
-Wal::Wal() {
-  fd = open(filepath, O_RDWR | O_CREAT, 0644);
-  if (fd == -1) {
+Wal::Wal(const char *d_path, const char *f_path)
+    : directory_path(d_path), file_path(f_path) {
+  fd = open(file_path, O_RDWR | O_CREAT, 0644);
+  if (fd == -1)
     throw std::runtime_error("file could not be opened");
-  }
+
+  int dir_fd{open(directory_path, O_DIRECTORY)};
+  if (dir_fd == -1)
+    throw std::runtime_error("directory could not be found");
+
+  if (fsync(dir_fd) == -1)
+    throw std::runtime_error("error calling fsync");
+
+  close(dir_fd);
 }
 
 Wal::~Wal() { close(fd); }
@@ -21,17 +31,33 @@ void Wal::append(OperationRecord op, std::span<std::byte> key,
   std::vector<std::byte> encode_result{RecordEncoder::encode(op, key, value)};
 
   // 2.write to the file
-  ssize_t bytes_written{write(fd, encode_result.data(), encode_result.size())};
-  // Should i assert that the bytes written are the same that the ones on the
-  // vector or should i leave that to replay
-  if (bytes_written == -1) {
-    throw std::length_error(
-        "this is a temporary simple first veriosn of the class");
+  size_t total_size{encode_result.size()};
+  size_t written{};
+  while (written < total_size) {
+    ssize_t bytes_written = write(fd, encode_result.data() + written,
+                                  encode_result.size() + written);
+
+    if (bytes_written == -1) {
+      throw std::length_error(
+          "this is a temporary simple first veriosn of the class");
+    }
+
+    written = bytes_written;
+    file_size += bytes_written;
   }
 
   // 3. force to flush with fsync i dont know if i should look a better strategy
   // for handling errors
   if (fsync(fd) == -1) {
-    throw std::runtime_error("Error calling fsync");
+    throw std::runtime_error("error calling fsync");
+  }
+}
+
+void Wal::replay() {
+  std::vector<std::byte> buffer(BUFFER_SIZE);
+  std::size_t bytes_read{};
+
+  while (bytes_read < file_size) {
+    ssize_t bytes = read(fd, buffer.data(), BUFFER_SIZE);
   }
 }
--- src/wal/wal.h
+++ src/wal/wal.h
@@ -5,11 +5,15 @@
 class Wal {
 private:
   // I need to establish a path for a temp file to dump the bytes
-  const char *filepath;
+  static constexpr std::size_t BUFFER_SIZE{4096};
+  std::size_t file_size;
+  const char *directory_path;
+  const char *file_path;
   int fd;
+  int file_pos;
 
 public:
-  Wal();
+  Wal(const char *d_path, const char *f_path);
   ~Wal();
   void append(OperationRecord op, std::span<std::byte> key,
               std::span<std::byte> value);
