c44c5cccacfa1a11557b529934e787d3fd6461fd
Cristian Blanco <cris0291@gmail.com>
2026-08-09 20:57:23 -0600

    add search entry

--- src/sstable/sstable.cpp
+++ src/sstable/sstable.cpp
@@ -1,5 +1,7 @@
 #include "sstable.h"
+#include "lsm_utilities.h"
 #include <cstddef>
+#include <cstdint>
 #include <fcntl.h>
 #include <span>
 #include <stdexcept>
@@ -29,23 +31,41 @@ Memtable::Record Sstable::read(std::vector<std::byte> key) {
   std::vector<std::byte> footer{};
   std::size_t footer_res{};
   ssize_t read_res{};
-
   std::vector<std::byte> index{};
+  std::vector<std::byte> records{};
+  std::size_t data_block_size{};
 
   ReadBlockResult footer_op_res{read_block(footer, FOOTER_SIZE, footer_offset)};
   if (footer_op_res == ReadBlockResult::ERROR) {
     throw std::runtime_error("something went wrong while reading the footer");
   }
 
-  std::uint64_t index_offset{
-      from_8_bytes_little_endian(std::span<std::byte>{footer}.subspan(0, 8))};
+  std::uint64_t index_offset{from_n_bytes_little_endian<std::uint64_t, 8>(
+      std::span<std::byte>{footer}.subspan(0, 8))};
 
-  std::uint64_t index_size{
-      from_8_bytes_little_endian(std::span<std::byte>{footer}.subspan(8, 8))};
+  std::uint64_t index_size{from_n_bytes_little_endian<std::uint64_t, 8>(
+      std::span<std::byte>{footer}.subspan(8, 8))};
 
   ReadBlockResult index_op_res{read_block(index, index_size, index_offset)};
   if (index_op_res == ReadBlockResult::ERROR) {
     throw std::runtime_error("something went wrong while reading the index");
+  }
+
+  std::vector<IndexEntry> index_entries{parse_index(index)};
+  std::size_t res{search_entry(index_entries, key)};
+
+  if (res == index_entries.size() - 1) {
+    data_block_size = index_entries[res].offset - index_offset;
+  } else {
+    std::size_t temp{res};
+    data_block_size = index_entries[res].offset - index_entries[++temp].offset;
+  }
+
+  ReadBlockResult records_op_res{
+      read_block(records, data_block_size, index_entries[res].offset)};
+  if (records_op_res == ReadBlockResult::ERROR) {
+    throw std::runtime_error(
+        "something went wrong while reading the data block");
   }
 };
 
@@ -55,18 +75,67 @@ std::vector<IndexEntry> Sstable::parse_index(std::vector<std::byte> &index) {
   const std::size_t key_size{4};
   IndexEntry index_entry{};
   std::span<std::byte> window;
+  std::vector<IndexEntry> res{};
 
   while (offset < index.size()) {
     std::span<std::byte> offset_window{
         std::span<std::byte>{index}.subspan(offset, offset_size)};
-    std::uint64_t off64{from_8_bytes_little_endian(offset_window)};
+    std::uint64_t off64{
+        from_n_bytes_little_endian<std::uint64_t, 8>(offset_window)};
     index_entry.offset = off64;
     offset += offset_size;
+
+    std::span<std::byte> key_len_window{
+        std::span<std::byte>{index}.subspan(offset, key_size)};
+    std::uint32_t key_len{
+        from_n_bytes_little_endian<std::uint32_t, 4>(key_len_window)};
+    offset += key_size;
+
+    std::vector<std::byte> key(index.begin() + offset, index.begin() + key_len);
+    index_entry.key = key;
+    res.push_back(index_entry);
+    offset += key_len;
+  }
+
+  return res;
+};
+
+std::size_t Sstable::search_entry(const std::vector<IndexEntry> &entries,
+                                  const std::vector<std::byte> &key) {
+  std::size_t low{0};
+  std::size_t high{entries.size() - 1};
+  std::size_t res{0};
+
+  while (low <= high) {
+    std::size_t mid = low + (high - low) / 2;
+    int comparison{compare_bytes(entries[mid].key, key)};
+    if (comparison <= 0) {
+      res = mid;
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return res;
+};
+
+void Sstable::search_records(std::vector<std::byte> &records) {
+  std::size_t curr_size{12};
+  std::size_t op_size{1};
+  std::size_t value_size{4};
+  std::uint32_t crc{from_n_bytes_little_endian<std::uint32_t, 4>(
+      std::span<std::byte>{records}.subspan(0, 4))};
+  std::uint32_t block_size{from_n_bytes_little_endian<std::uint32_t, 4>(
+      std::span<std::byte>{records}.subspan(0, 4))};
+  std::uint32_t record_count{from_n_bytes_little_endian<std::uint32_t, 4>(
+      std::span<std::byte>{records}.subspan(8, 4))};
+
+  while (curr_size < block_size) {
   }
 };
 
 ReadBlockResult Sstable::read_block(std::vector<std::byte> &block,
-                                    std::size_t size, std::size_t offset) {
+                                    std::uint64_t size, std::uint64_t offset) {
   std::size_t block_result{};
   ssize_t read_result{};
 
@@ -82,19 +151,4 @@ ReadBlockResult Sstable::read_block(std::vector<std::byte> &block,
   }
 
   return ReadBlockResult::GOOD;
-};
-
-std::uint64_t
-Sstable::from_8_bytes_little_endian(std::span<std::byte> bytes_to_copy) {
-  std::uint64_t value64 =
-      std::to_integer<std::uint64_t>(bytes_to_copy[0]) |
-      (std::to_integer<std::uint64_t>(bytes_to_copy[1]) << 8) |
-      (std::to_integer<std::uint64_t>(bytes_to_copy[2]) << 16) |
-      (std::to_integer<std::uint64_t>(bytes_to_copy[3]) << 24) |
-      (std::to_integer<std::uint64_t>(bytes_to_copy[4]) << 32) |
-      (std::to_integer<std::uint64_t>(bytes_to_copy[5]) << 40) |
-      (std::to_integer<std::uint64_t>(bytes_to_copy[6]) << 48) |
-      (std::to_integer<std::uint64_t>(bytes_to_copy[7]) << 52);
-
-  return value64;
 };
--- src/sstable/sstable.h
+++ src/sstable/sstable.h
@@ -3,7 +3,6 @@
 #include "memtable.h"
 #include "sstable_operation.h"
 #include <cstddef>
-#include <cstdint>
 #include <span>
 #include <string>
 #include <vector>
@@ -13,10 +12,22 @@ private:
   static constexpr std::size_t FOOTER_SIZE{20};
   int fd;
   std::size_t file_size;
-  std::uint64_t from_8_bytes_little_endian(std::span<std::byte> byes_to_copy);
+  template <typename T, std::size_t N>
+  T from_n_bytes_little_endian(std::span<std::byte> bytes_to_copy) {
+    T value_n{T{}};
+
+    for (int i{}; i < N; i++) {
+      value_n |= (std::to_integer<T>(bytes_to_copy[i]) << (i * 8));
+    }
+
+    return value_n;
+  }
   ReadBlockResult read_block(std::vector<std::byte> &block, std::size_t size,
                              std::size_t offset);
   std::vector<IndexEntry> parse_index(std::vector<std::byte> &index);
+  std::size_t search_entry(const std::vector<IndexEntry> &entries,
+                           const std::vector<std::byte> &key);
+  void search_records(std::vector<std::byte> &records);
 
 public:
   Sstable(std::string path);
